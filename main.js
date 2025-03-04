6979d7740690571c35975da0577112a32e9d37f4
LAPT-CSH-30 <cshtd.computacion@gmail.com>
2025-03-04 09:20:02 -0600

    second commit

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 //conexión con HTML (Seleccionar elementos)
 
+const songForm = document.getElementById('song_form');
+const playList = document.getElementById('play_list');
+
+//Función para agregar una canción a la lista
+function addSong(songName, artistName,  songUrl) {
+    const listSong = document.createElement('li');
+    listSong.innerHTML = `
+         <strong> ${songName}</strong>
+         - ${artistName}
+         <a href="${songUrl}" target="_blank">Reproducir</a>
+         <button class="delete-btn">Eliminar</button>
+         
+    `;
+    //agregar al html
+    playList.appendChild(listSong);
+
+}
+//Manejo de envio de formulario (eventos) 
+
+songForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    //obtener datos del formulario
+    const songName = document.getElementById("song_name").value;
+    const artistName= document.getElementById("artist_name").value;
+    const songUrl = document.getElementById("url_song").value;
+
+    addSong(songName,artistName,songUrl);
+
+    songForm.reset();
+})
+
